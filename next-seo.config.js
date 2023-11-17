bfbaa82459eef5fbda9bf52f94385f65c547956e
cani <78512922+canii@users.noreply.github.com>
2023-11-17 13:16:16 +0300

    Update next-seo.config.js

--- next-seo.config.js
+++ next-seo.config.js
@@ -1,27 +1,27 @@
 const defaultSEOConfig = {
-  title: "Anish De | Fullstack Developer",
+  title: "Kerem | Front-End Developer",
   description:
-    "10th grader • Fullstack Web Developer • Web3 Enthusiast • Blogger • Tech Enthusiast",
-  cannonical: "https://anishde.dev/",
+    "2th grader • Front-End Developer • Graphic Designer • PlayBoy • E-Trader",
+  cannonical: "https://kerem.app/",
   openGraph: {
     type: "website",
-    url: "https://anishde.dev/",
-    site_name: "Anish De",
-    title: "Anish De | Fullstack Developer",
+    url: "https://kerem.app/",
+    site_name: "Kerem",
+    title: "Kerem | Front-End Developer",
     description:
-      "10th grader • Fullstack Web Developer • Web3 Enthusiast • Blogger • Tech Enthusiast",
+      "2th grader • Front-End Developer • Graphic Designer • PlayBoy • E-Trader",
     images: [
       {
         url: "https://res.cloudinary.com/anishde12020/image/upload/v1654360780/Blogfolio/og.png",
         width: 1200,
         height: 630,
-        alt: "Anish De's Portfolio Site's Opengraph Image",
+        alt: "Kerem's Portfolio",
       },
     ],
     profile: {
-      firstName: "Anish",
-      lastName: "De",
-      username: "AnishDe12020",
+      firstName: "Kerem",
+      lastName: "Can",
+      username: "jackim",
       gender: "male",
     },
   },
