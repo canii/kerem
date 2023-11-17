2ac3f5848d684b031f42ccf6f3f45d28d591c2ee
cani <78512922+canii@users.noreply.github.com>
2023-11-17 12:55:28 +0300

    Update Hero.tsx

--- components/Home/Hero/Hero.tsx
+++ components/Home/Hero/Hero.tsx
@@ -7,15 +7,15 @@ const Hero = (): JSX.Element => {
     <div className="flex flex-col-reverse md:flex-row md:justify-between md:space-y-0">
       <div className="mt-8 flex flex-col justify-center space-y-4 md:mt-0 md:w-2/3">
         <h1 className="will-change-opacity text-5xl font-bold will-change-transform">
-          Anish De
+          Kerem
         </h1>
         <h2 className="will-change-opacity text-lg font-medium text-gray-400 will-change-transform">
-          10th grader • Fullstack Web Developer • Web3 Enthusiast • Blogger •
-          Tech Enthusiast
+          10th grader • Front-end Developer • Graphic Designer • PlayBoy •
+          E-Trader
         </h2>
         <p className="text-md will-change-opacity text-lg text-gray-300 will-change-transform">
-          I am a fullstack web developer, currently looking into learning new
-          things (Rust, Go, Web3 and backend technologies). Whenever I feel like
+          I am a front-end developer, currently looking into learning new
+          things (Rust, , Web2 and front-end technologies). Whenever I feel like
           I should share my knowledge with others, I like to do it via blog
           posts and social media.
         </p>
@@ -26,7 +26,7 @@ const Hero = (): JSX.Element => {
           src={AvatarJPG}
           layout="fill"
           placeholder="blur"
-          alt="Anish De"
+          alt="Kerem"
         />
       </div>
     </div>
