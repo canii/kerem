3e4d2f8549d0ce0c080f5f67263b0a30c3b51f02
cani <78512922+canii@users.noreply.github.com>
2023-11-17 13:38:06 +0300

    Update next-seo.config.js

--- next-seo.config.js
+++ next-seo.config.js
@@ -12,7 +12,7 @@ const defaultSEOConfig = {
       "2th grader • Front-End Developer • Graphic Designer • PlayBoy • E-Trader",
     images: [
       {
-        url: "https://res.cloudinary.com/anishde12020/image/upload/v1654360780/Blogfolio/og.png",
+        url: "https://cdn.discordapp.com/attachments/1141150503963729960/1175021970534965288/Baslksz-3.png?ex=6569b77c&is=6557427c&hm=ffca2e22f7a6fbeb7e8b854520ce193d8373c67f73ab5a4350c3b95cce55c2a4&",
         width: 1200,
         height: 630,
         alt: "Kerem's Portfolio",
