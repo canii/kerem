073278071925d5dc8379c605920e8cb3037ac623
cani <78512922+canii@users.noreply.github.com>
2023-11-19 13:57:52 +0300

    Update socials.tsx

--- data/socials.tsx
+++ data/socials.tsx
@@ -20,25 +20,25 @@ const socials: Social[] = [
     id: "instagram",
     name: "Instagram",
     url: "https://instagram.com/_kermcann",
-    icon: <Twitterlogo color="#1DA1F2" />,
+    icon: <Twitterlogo />,
   },
   {
     id: "spotify",
     name: "Spotify",
     url: "https://open.spotify.com/user/pbf8r6cuik3evk5kio58x0uue?si=1349beef38e54e16",
-    icon: <GitHubLogo />,
+    icon: <GitHubLogo  color="#1DB954"/>,
   },
   {
     id: "discord",
     name: "Discord",
     url: "https://discord.com/users/314403590942752778/",
-    icon: <YouTubeLogo color="#ff0000" />,
+    icon: <YouTubeLogo color="#7289da" />,
   },
   {
     id: "steam",
     name: "Steam",
     url: "https://steamcommunity.com/id/jackaktus",
-    icon: <HashnodeLogo color="#2962ff" />,
+    icon: <HashnodeLogo color="#171a21" />,
   },
 ];
 
